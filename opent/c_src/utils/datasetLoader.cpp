cf72c4ea49a4979565f0bd45e7bb8eebd56d93d1
Birkenpapier <alexander.kluender@web.de>
2022-04-18 00:11:48 +0200

    added first version of dataset loader util and baseCNN

---
+++ opent/c_src/utils/datasetLoader.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+#include <filesystem>
+#include <vector>
+
+
+int dataset(std::string path) {
+    if(path.length() <= 2) {
+        return 0;
+    }
+    
+   return 0;
+}
